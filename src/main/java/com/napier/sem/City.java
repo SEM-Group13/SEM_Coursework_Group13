ab8de41ddcbaeff7797d6ac4d11009ac400b0232
Saaaaaaaaaaaam <40437441@live.napier.ac.uk>
2021-03-19 23:57:48 +0000

    added new classes

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,32 @@
+package com.napier.sem;
+
+/**
+ * Representation of a City
+ */
+
+public class City {
+    /**
+     * City ID Number
+     */
+    public int city_no;
+
+    /**
+     * Name of City
+     */
+    public String name;
+
+    /**
+     * 3 Character Identification Code from country
+     */
+    public String country;
+
+    /**
+     * City's district
+     */
+    public String district;
+
+    /**
+     * Population of the City
+     */
+    public int population;
+}
