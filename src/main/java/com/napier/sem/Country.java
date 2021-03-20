81801ff1cec95f08eace29ea49005e133b43cf56
Saaaaaaaaaaaam <40437441@live.napier.ac.uk>
2021-03-20 00:58:22 +0000

    filled out classes

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -1,4 +1,79 @@
 package com.napier.sem;
 
 public class Country {
+    /**
+     * 3 Digit country ID code
+     */
+    public String code;
+
+    /**
+     * Name of country
+     */
+    public String name;
+
+    /**
+     * Continent where country is found
+     */
+    public String continent;
+
+    /**
+     * Region where country is found
+     */
+    public String region;
+
+    /**
+     * Surface area of land owned by the country
+     */
+    public String area;
+
+    /**
+     * Year of countries independence
+     */
+    public int indep_year;
+
+    /**
+     * Population of country
+     */
+    public int population;
+
+    /**
+     * Life expectancy of the country
+     */
+    public double life_expectancy;
+
+    /**
+     * Gross national product
+     */
+    public double gnp;
+
+    /**
+     * Gross national product (old)
+     */
+    public double gnpold;
+
+    /**
+     * Name for country in local language
+     */
+    public String local_name;
+
+    /**
+     * Type of government in country
+     */
+    public String government;
+
+    /**
+     * Head of state
+     */
+    public String head_of_state;
+
+    /**
+     * ID number for capital city
+     */
+    public int capital;
+
+    /**
+     * 2 digit ID code for country
+     */
+    public String code2;
+
 }
--- src/main/java/com/napier/sem/CountryLanguage.java
+++ src/main/java/com/napier/sem/CountryLanguage.java
@@ -1,4 +1,25 @@
 package com.napier.sem;
 
 public class CountryLanguage {
+    /**
+     * 3 digit ID code for country
+     */
+    public String country;
+
+    /**
+     * Spoken Language
+     */
+    public String language;
+
+    /**
+     * Is the language official
+     * Data will be stored as char to accurately reflect data
+     * in the database
+     */
+    public char is_official_char;
+
+    /**
+     * Percentage of speakers in country
+     */
+    public double percentage;
 }
